44eed09ee5f91212ae7d3aa4848540459efab2ed
Roy Sprowl <roys@jetstreamsoftware.com>
2019-06-14 15:06:58 -0700

    Update for new Docker repo

--- device-telemetry/scripts/docker/publish.cmd
+++ device-telemetry/scripts/docker/publish.cmd
@@ -3,9 +3,9 @@
 @ECHO off & setlocal enableextensions enabledelayedexpansion
 
 :: Note: use lowercase names for the Docker images
-SET DOCKER_IMAGE="aivideo/telemetry-dotnet"
+SET DOCKER_IMAGE="azureaivideo/telemetry-dotnet"
 :: "testing" is the latest dev build, usually matching the code in the "master" branch
-SET DOCKER_TAG=%DOCKER_IMAGE%:testing
+SET DOCKER_TAG=%DOCKER_IMAGE%:3.0.0
 
 docker push %DOCKER_TAG%
 
